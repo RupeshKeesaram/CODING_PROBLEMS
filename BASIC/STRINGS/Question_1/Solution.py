bbb41e0c2736a98f6982197e8964777085d350a4
Rupesh Keesaram <75531784+RupeshKeesaram@users.noreply.github.com>
2022-07-28 23:16:46 +0530

    Create Solution.py

---
+++ BASIC/STRINGS/Question_1/Solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        ans=""
+        for i in s:
+            if i.isalnum():
+                ans+=i.lower()
+        return ans==ans[::-1]
